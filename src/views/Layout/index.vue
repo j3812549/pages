2c2f587f218cf6639e8cbaff0adfb80c0550aab9
hyx <380012546@qq.com>
2024-02-06 11:30:34 +0800

    【A】添加地址

--- src/views/Layout/index.vue
+++ src/views/Layout/index.vue
@@ -7,8 +7,14 @@ import { RouterLink } from 'vue-router'
     <h3 class="text-xl m-2 font-medium">插件列表</h3>
     <ul>
       <li>
-        <RouterLink to="/vuekeepcachescrollhooks">vue-keep-cache-scroll-hooks</RouterLink>
-        <span class="ml-2">keepalive下缓存滚动条位子的hooks</span>
+        <div>
+          <RouterLink to="/vuekeepcachescrollhooks">vue-keep-cache-scroll-hooks</RouterLink>
+          <span class="ml-2">keepalive下缓存滚动条位子的hooks</span>
+          <span class="ml-2"><a class="font-bold"
+              href="https://github.com/j3812549/vue-keep-cache-scroll-hooks">github</a></span>
+          <span class="ml-2"><a class="font-bold"
+              href="https://www.npmjs.com/package/vue-keep-cache-scroll-hooks">npm</a></span>
+        </div>
       </li>
     </ul>
   </div>
